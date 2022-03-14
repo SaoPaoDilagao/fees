9220578506f17dc1290b44bdfc0c32befc3e9419
Luigi Teglia <teglia.luigi@gmail.com>
2022-03-13 21:29:37 -0500

    Added  some functions in the service layer

--- src/main/java/com/nttdata/fees/service/FeesService.java
+++ src/main/java/com/nttdata/fees/service/FeesService.java
@@ -1,14 +1,19 @@
 package com.nttdata.fees.service;
 
+import com.nttdata.fees.dto.request.FeeRequest;
 import com.nttdata.fees.entity.Fee;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface FeesService {
 	
 	Flux<Fee> listByIdTransaction(String idTransaction);
 	Flux<Fee> listByProductNumber(String productNumber);
 	Flux<Fee> listByClientDocumentNumber(String clientDocumentNumber);
-	
+	Mono<Void> deleteFees(FeeRequest request);
+	Mono<Void> createFees(FeeRequest request);
+	Mono<Void> updateFee(FeeRequest request);
+  	
 
 }
--- src/main/java/com/nttdata/fees/service/FeesServiceImpl.java
+++ src/main/java/com/nttdata/fees/service/FeesServiceImpl.java
@@ -2,11 +2,13 @@ package com.nttdata.fees.service;
 
 import org.springframework.stereotype.Service;
 
+import com.nttdata.fees.dto.request.FeeRequest;
 import com.nttdata.fees.entity.Fee;
 import com.nttdata.fees.repository.FeesRepository;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +29,24 @@ public class FeesServiceImpl implements FeesService{
 
 	@Override
 	public Flux<Fee> listByClientDocumentNumber(String clientDocumentNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<Void> deleteFees(FeeRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<Void> createFees(FeeRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<Void> updateFee(FeeRequest request) {
 		// TODO Auto-generated method stub
 		return null;
 	}
